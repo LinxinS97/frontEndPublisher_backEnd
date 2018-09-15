32d3a7489ce94514debd24fc1b5032f3d2851a41
elpis <stranger469@icloud.com>
2018-09-15 12:46:20 +0800

    合并冲突2

--- src/plugin/command.js
+++ src/plugin/command.js
@@ -11,7 +11,7 @@ module.exports = command => {
             return stdErr;
         } else {
             console.log(stdOut);
-            return out;
+            return stdOut;
         }
     });
 }
