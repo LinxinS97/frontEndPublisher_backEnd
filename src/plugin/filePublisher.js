bdfd9eac335c5445c949837d252d4c0fc8f9f8a5
Alex <stranger469@icloud.com>
2018-09-25 13:40:18 +0800

    sftp

--- src/plugin/filePublisher.js
+++ src/plugin/filePublisher.js
@@ -20,7 +20,7 @@ const getFile = async (filePath, sftp, remotePath) => {
         if(stats.isFile()) {
             try{
                 console.log(filename);
-                await sftp.put(filename, remotePath);
+                await sftp.put('.' + filedir.split('/frontEndPublisher/build')[1] + filename, remotePath);
             } catch(e) {
                 console.error(e);
             }
