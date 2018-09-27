737c29f8ef593d02c675a13cf06e0ac65347d5bc
Alex <stranger469@icloud.com>
2018-09-27 10:51:58 +0800

    pull

--- src/plugin/git_api.js
+++ src/plugin/git_api.js
@@ -1,13 +1,22 @@
 const Git = require('nodegit');
 const path = require('path');
 const APIError = require('../middle/rest').APIError;
-
+const opts = {
+    fetchOpts: {
+        callbacks: {
+            certificateCheck: () => 1,
+            credentials: function (url, userName) {
+                return Git.Cred.sshKeyFromAgent(userName);
+            }
+        }
+    }
+};
 module.exports = {
     async clone(url) {
         // TODO: 新建项目，通过url clone项目并保存到对应地址
         const name = url.split('/').pop().split('.')[0];
         try {
-            await Git.Clone(url, path.resolve('./repos/' + name));
+            await Git.Clone(url, path.resolve('./repos/' + name), opts);
         } catch (e) {
             console.log(e);
             throw new APIError('git:clone error', e);
@@ -20,17 +29,18 @@ module.exports = {
     async pull(name, username, psw) {
         try {
             const repo = await Git.Repository.open(path.resolve('./repos/' + name));
-            await repo.fetchAll({
-                certificateCheck: () => 1,
-                credentials: (url, userName) => {
-                    if(username === null && psw === null) {
-                        return Git.Cred.sshKeyFromAgent(userName);
-                    } else {
-                        console.log(username, psw);
-                        return Git.Cred.userpassPlaintextNew(username, psw);
-                    }
-                },
-            }, true);
+            // await repo.fetchAll({
+            //     certificateCheck: () => 1,
+            //     credentials: (url, userName) => {
+            //         if(username === null && psw === null) {
+            //             return Git.Cred.sshKeyFromAgent(userName);
+            //         } else {
+            //             console.log(username, psw);
+            //             return Git.Cred.userpassPlaintextNew(username, psw);
+            //         }
+            //     },
+            // });
+            await repos.fetchAll(opts);
             await repo.mergeBranches('master', 'origin/master');
         } catch (e) {
             console.error(e);
