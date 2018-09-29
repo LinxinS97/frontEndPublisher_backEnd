915790fdc63ab0055c218a89053e2c238934062f
Alex <stranger469@icloud.com>
2018-09-29 16:23:58 +0800

    make fastPut, mkdir asynchronous

--- src/plugin/filePublisher.js
+++ src/plugin/filePublisher.js
@@ -18,17 +18,38 @@ module.exports = async function getFile(filePath, sftp, remotePath) {
         let filedir = path.join(filePath, filename);
         const stats = fs.statSync(filedir);
         if(stats.isFile()) {
-            sftp.fastPut(filedir, remotePath + '/' + filename, (err) => {
-                if(err) throw new APIError('file put error:can not put file', err);
-            });
+            // await sftp.fastPut(filedir, remotePath + '/' + filename, (err) => {
+            //     if(err) throw new APIError('file put error:can not put file', err);
+            // });
+            await fastPut(sftp, filedir, remotePath + '/' + filename)
             console.log(filedir);
         }
         if(stats.isDirectory()){
             // 更新目录
             let newPath = remotePath + filedir.split(filePath)[1];
             console.log(filedir, newPath);
-            await sftp.mkdir(newPath);
+            await mkdir(newPath);
             await getFile(filedir, sftp, newPath);
         }
     }
 };
+
+// 用Promise重写fastPut
+function fastPut(sftp, filedir, remotePath) {
+    return new Promise((resolve, reject) => {
+        sftp.fastPut(filedir, remotePath, function (err) {
+            if (err) return reject(err);
+            return resolve(`${localPath} was successfully uploaded to ${remotePath}!`);
+        });
+    });
+}
+
+// 用promise重写mkdir
+function mkdir(sftp, newPath) {
+    return new Promise((resolve, reject) => {
+        sftp.mkdir(newPath, err => {
+            if(err) return reject(err);
+            return resolve(`${newPath} directory has been created`);
+        });
+    })
+}
